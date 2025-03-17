6d68cb7a636d9c27d9fd31d2b6f5d37b2ffe7e3d
BartCatalano <catalanobartolomeo@gmail.com>
2025-03-17 12:15:31 +0100

    first commit

---
+++ src/main.java
@@ -0,0 +1,9 @@
+import org.snack.java.student.studente;
+
+public class main {
+public static void main(String[] args) {
+    studente studente1 = new studente("Mario", "Rossi", 25);
+    System.out.println(studente1.toString());
+   
+}
+}
---
+++ src/org/snack/java/student/studente.java
@@ -0,0 +1,49 @@
+package org.snack.java.student;
+
+public class studente {
+
+    // creo le var da popolare
+private String nome;
+private String cognome;
+private int age;
+
+// creo il costruttore 
+
+public studente(String nome, String cognome, int age) {
+    this.nome = nome;
+    this.cognome = cognome;
+    this.age = age;
+}
+
+public String getnome(){
+    return this.nome;
+}
+
+public void setnome(String nome){
+    this.nome = nome;
+}
+
+public String getcognome(){
+    return this.cognome;
+}
+
+public void setcognome(String cognome){
+    this.cognome = cognome;
+}
+
+public int getage(){
+    return this.age;
+}
+
+public void setetà(int age){
+    this.age = age;
+}
+
+public String toString(){
+    return "Studente [" + nome + "" + " " +" " + cognome + " "+ " ," + "" + age + "]";
+}
+
+}
+
+
+
